e6d0a770480112da16f819da8e608bb66155f045
reytang <nubaboub@gmail.com>
2023-02-27 05:44:45 -0800

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updating th value it points to
+ * @n: the interger
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+files for 0x05-pointers_arrays_strings
